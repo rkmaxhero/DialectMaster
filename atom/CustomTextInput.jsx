e02584ab3a73713c665fc06b4a895d429e089b1f
rkmaxhero <45477562+rkmaxhero@users.noreply.github.com>
2024-03-15 12:33:21 -0400

    Creating standardized custom button & input field
    These could later be used to make a custom form component.

---
+++ atom/CustomTextInput.jsx
@@ -0,0 +1,29 @@
+import { StyleSheet, TextInput, Text} from 'react-native';
+
+
+export default function (props) {
+    return(
+        <>
+        <Text style={styles.label}>{props.label}</Text>
+        <TextInput style={styles.field}>{props.placeholder}</TextInput>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    field: {
+        backgroundColor: '#EEE462',
+        width: '100%',
+        height: 57,
+        borderRadius: 24, 
+        padding: 20,
+
+        fontSize: 18
+    },
+
+    label: {
+        fontSize: 24, 
+        fontWeight: 600,
+        marginBottom: 10,
+    }
+});
